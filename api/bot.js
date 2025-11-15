8cbe2995218e7b44e89efc012a3807dd5b22ff5c
Watsoniosin <watsonvitaliy@gmail.com>
2025-11-15 22:16:39 +0200

    Update bot.js

--- api/bot.js
+++ api/bot.js
@@ -1,11 +1,9 @@
-// api/bot.js
 import { Telegraf } from 'telegraf';
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-// Команда /start
 bot.start((ctx) => {
-  ctx.reply('Привіт! Натисни Menu 👇', {
+  ctx.reply('Привіт! Натисни Menu', {
     reply_markup: {
       keyboard: [[{ text: 'Menu', web_app: { url: 'https://giftify-ua1.vercel.app' } }]],
       resize_keyboard: true
@@ -13,18 +11,15 @@ bot.start((ctx) => {
   });
 });
 
-// Запуск
 export default async function handler(req, res) {
   if (req.method === 'POST') {
-    await bot.handleUpdate(req.body, res);
+    await bot.handleUpdate(req.body);
+    res.status(200).end();
   } else {
     res.status(200).json({ message: 'Giftify UA Bot is running!' });
   }
 }
 
-// Для Vercel
 export const config = {
-  api: {
-    bodyParser: false
-  }
+  api: { bodyParser: false }
 };
